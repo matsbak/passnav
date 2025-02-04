5e3301cd2b903515717ca9630a9b9cc0ca971801
Mats Bakketeig <matsbak@stud.ntnu.no>
2025-02-04 18:58:38 +0100

    Edited runner class

--- src/main/java/matsbak/app/passnav_api/PassnavApiApplication.java
+++ src/main/java/matsbak/app/passnav_api/PassnavApiApplication.java
@@ -2,12 +2,19 @@ package matsbak.app.passnav_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
+/**
+ * The PassnavApiApplication class represents the runner class for the application.
+ * 
+ * @author Mats Bakketeig
+ * @version v1.0 (2025.02.04)
+ */
 @SpringBootApplication
 public class PassnavApiApplication {
-
 	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(PassnavApiApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter("passnav.pid"));
 		SpringApplication.run(PassnavApiApplication.class, args);
 	}
-
 }
