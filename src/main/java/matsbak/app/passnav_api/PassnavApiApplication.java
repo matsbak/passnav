db3ea4e8d06c1fceda4ea62cae37b0679494768a
Mats Bakketeig <matsbak@stud.ntnu.no>
2025-02-04 13:42:36 +0100

    Added fresh Spring Boot project

---
+++ src/main/java/matsbak/app/passnav_api/PassnavApiApplication.java
@@ -0,0 +1,13 @@
+package matsbak.app.passnav_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassnavApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassnavApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matsbak/app/passnav_api/PassnavApiApplicationTests.java
@@ -0,0 +1,13 @@
+package matsbak.app.passnav_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassnavApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
