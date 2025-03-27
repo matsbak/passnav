4ecfe0e2f754a17f12fbcef3cfe527433b250e01
Mats Bakketeig <mats.bakketeig@outlook.com>
2025-03-27 21:31:18 +0100

    Refactored starting classes

---
+++ src/main/java/matsbak/app/PassnavApp.java
@@ -0,0 +1,13 @@
+package matsbak.app;
+
+/**
+ * The PassnavApp class represents the runner class for the application.
+ * 
+ * @author Mats Bakketeig
+ * @version v0.0.1 (2025.03.27)
+ */
+public class PassnavApp {
+  public static void main(String[] args) {
+    PassnavApp.main(args);
+  }
+}
