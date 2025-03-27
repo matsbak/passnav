f79063e5cd59ab76d4089bafd504ff5e298ea4b1
Mats Bakketeig <mats.bakketeig@outlook.com>
2025-03-27 21:07:20 +0100

    Added starting classes

---
+++ src/main/java/matsbak/app/PassnavLauncher.java
@@ -0,0 +1,20 @@
+package matsbak.app;
+
+import matsbak.app.view.PassnavApp;
+
+/**
+ * The PassnavLauncher class represents the runner class for the application and contains its main
+ * starting point.
+ * 
+ * @author Mats Bakketeig
+ * @version v0.0.1 (2025.03.27)
+ */
+public class PassnavLauncher {
+  private PassnavLauncher() {
+    // Intentionally left blank
+  }
+
+  public static void main(String[] args) {
+    PassnavApp.main(args);
+  }
+}
---
+++ src/main/java/matsbak/app/view/PassnavApp.java
@@ -0,0 +1,17 @@
+package matsbak.app.view;
+
+/**
+ * The PassnavApp class represents the starting point for the TUI.
+ * 
+ * @author Mats Bakketeig
+ * @version v0.0.1
+ */
+public class PassnavApp {
+  private PassnavApp() {
+    // Intentionally left blank
+  }
+
+  public static void main(String[] args) {
+    // Launch TUI
+  }
+}
